dbd0f9497a5ca20bf3aaa0aea68b0d3abe5c8897
ramnathbishwas <rambis075@gmail.com>
2022-09-26 10:10:43 +0530

    Update Doubly LinkedList Implementation2.js

--- Doubly LinkedList Implementation2.js
+++ Doubly LinkedList Implementation2.js
@@ -73,6 +73,24 @@ class DoublyLinkedList {
     }
     return currentNode;
   }
+  
+  reverse(){
+    if(!this.head.next){
+      return this.head;
+}
+    let first = this.head;
+    this.tail= this.head;
+    let second =first.next;
+    while(second){
+      const temp=second.next;
+      second.next = first;
+      first =second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head=first;
+  }
+
 }
 
 let myLinkedList = new DoublyLinkedList(10);
